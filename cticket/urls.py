3962282eea1e8cd6ceaa7e36c7bd04183e9057e1
Anton Backer <olegov@gmail.com>
2015-06-07 23:24:04 -0400

    Add H5BP, bootstrap, and a bit of color

--- cticket/urls.py
+++ cticket/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,4 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+] + staticfiles_urlpatterns()
